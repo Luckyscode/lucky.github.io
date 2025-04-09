cf75d5f1794e18d05eced9f8d0fd1d4d4562c439
LuckyCodes <131615884+Luckyscode@users.noreply.github.com>
2025-04-09 22:46:19 +0200

    feat: improve navbar functionality and  styles

---
+++ index.js
@@ -0,0 +1,11 @@
+// This is the JavaScript code for the index.html file
+// This code handles the functionality of a navigation menu toggle
+$(".nav-toggle").click(function(){
+    if ($(".nav-toggle").attr("src") === "assets/menu.svg") {
+    $(".nav-toggle").attr("src","assets/menuactive.svg")
+    $(".navitems").css("display","flex")}
+    else {
+        $(".nav-toggle").attr("src","assets/menu.svg")
+        $(".navitems").css("display","none")
+    }
+  });
